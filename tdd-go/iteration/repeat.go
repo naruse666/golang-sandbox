8b09024e20e435ee8690631855221b873a7a8ebf
naruse666 <0x706f6b6f@gmail.com>
2024-07-22 22:57:09 +0900

    tdd go: iteration

---
+++ tdd-go/iteration/repeat.go
@@ -0,0 +1,10 @@
+package iteration
+
+func Repeat(character string, repeatCount int) string {
+	var repeated string
+
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
