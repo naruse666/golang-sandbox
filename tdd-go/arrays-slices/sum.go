1c9fb344c5ccf2232ee1a394c4cf6d8a5a0f8c26
naruse666 <0x706f6b6f@gmail.com>
2024-08-01 15:13:33 +0900

    tdd go: arrays-slices

---
+++ tdd-go/arrays-slices/sum.go
@@ -0,0 +1,29 @@
+package main
+
+func Sum(numbers []int) int {
+	var s int
+	for _, v := range numbers {
+		s += v
+	}
+	return s
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
