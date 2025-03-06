dc78a862b72720a230ad1fbfd9ea017e2aaccc55
naruse666 <0x706f6b6f@gmail.com>
2025-03-06 13:43:43 +0900

    real-world-http: simple get with query

--- real-world-http/simple-get/main.go
+++ real-world-http/simple-get/main.go
@@ -4,14 +4,20 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	resp, err := http.Get("http://localhost:18888")
+	values := url.Values{
+		"query": {"hello world"},
+	}
+
+	resp, err := http.Get("http://localhost:18888" + "?" + values.Encode())
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
