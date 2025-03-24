9341b0bc9479ebe4db036bfe3528805549323a6e
naruse666 <0x706f6b6f@gmail.com>
2025-03-24 19:34:02 +0900

    grpc: client

---
+++ grpc/myapp/cmd/client/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	hellopb "mygrpc/pkg/grpc"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	scanner *bufio.Scanner
+	client  hellopb.GreetingServiceClient
+)
+
+func main() {
+	fmt.Println("start gRPC Client.")
+
+	scanner = bufio.NewScanner(os.Stdin)
+
+	address := "localhost:8080"
+
+	conn, err := grpc.NewClient(
+		address,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatal("Connection failed.")
+		return
+	}
+	defer conn.Close()
+	conn.Connect()
+
+	client = hellopb.NewGreetingServiceClient(conn)
+
+	for {
+		fmt.Println("1: send Request")
+		fmt.Println("2: exit")
+		fmt.Print("please enter >")
+
+		scanner.Scan()
+		in := scanner.Text()
+
+		switch in {
+		case "1":
+			Hello()
+		case "2":
+			fmt.Println("bye.")
+			goto M
+		}
+	}
+M:
+}
+
+func Hello() {
+	fmt.Println("Please enter your name.")
+	scanner.Scan()
+	name := scanner.Text()
+
+	req := &hellopb.HelloRequest{
+		Name: name,
+	}
+
+	res, err := client.Hello(context.Background(), req)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res.GetMessage())
+	}
+}
