d0e73a6dcd335923555e18b321c3461cc9751fb9
naruse666 <0x706f6b6f@gmail.com>
2025-03-24 18:58:11 +0900

    grpc: server

---
+++ grpc/myapp/cmd/server/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	hellopb "mygrpc/pkg/grpc"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type myServer struct {
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	return &hellopb.HelloResponse{
+		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	}, nil
+}
+
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+func main() {
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
---
+++ grpc/myapp/go.mod
@@ -0,0 +1,13 @@
+module mygrpc
+
+go 1.24.1
+
+require (
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250313205543-e70fdf4c4cb4 // indirect
+	google.golang.org/grpc v1.71.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
+)
