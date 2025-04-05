3f13ae0cf49c87b50733760fb877d4376185c419
naruse666 <0x706f6b6f@gmail.com>
2025-04-05 10:16:28 +0900

    browser: print class

--- gui/main.go
+++ gui/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"image/color"
 	"log"
+	// "net/http"
+	"os"
 	"strings"
+
+	"github.com/kbinani/screenshot"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/text"
@@ -14,18 +20,34 @@ import (
 )
 
 type Game struct {
-	texts []string
+	lines   []Line
+	scrollY int
 }
 
 func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		g.scrollY += 4
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		g.scrollY -= 4
+	}
+	// mouse wheel
+	_, dy := ebiten.Wheel()
+	g.scrollY -= int(dy * 20)
+	// スクロール範囲の制限
+	if g.scrollY < 0 {
+		g.scrollY = 0
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	face := basicfont.Face7x13
 	y := 30
-	for _, line := range g.texts {
-		text.Draw(screen, line, face, 20, y, color.White)
+	for _, line := range g.lines {
+		// x := 20 + line.Depth*20
+		x := 20
+		text.Draw(screen, line.Text, face, x, y-g.scrollY, color.White)
 		y += 20
 	}
 }
@@ -34,34 +56,64 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
-	doc, err := html.Parse(strings.NewReader("<html><body><p>hello</p><div>Div</div></body></html>"))
+	data, err := os.Open("test.html")
+	if err != nil {
+		panic(err)
+	}
+	doc, err := html.Parse(bufio.NewReader(data))
+	// doc, err := html.Parse(strings.NewReader("<html><body><p>hello</p><div>Div</div></body></html>"))
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var texts []string
-	extractPTexts(doc, &texts)
+	fmt.Printf("%#v", doc)
+	var lines []Line
+	walkDOM(doc, 0, &lines)
 
-	game := &Game{texts: texts}
-	ebiten.SetWindowSize(640, 480)
+	game := &Game{lines: lines}
+	width, height := getPrimaryScreenSize()
+	ebiten.SetWindowSize(width, height)
 	ebiten.SetWindowTitle("test window")
 	if err := ebiten.RunGame(game); err != nil {
 		panic(err)
 	}
 }
 
-func extractPTexts(n *html.Node, results *[]string) {
-	if n.Type == html.ElementNode {
-		if n.Data == "p" || n.Data == "div" {
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				if c.Type == html.TextNode {
-					*results = append(*results, c.Data)
-				}
+type Line struct {
+	Text  string
+	Depth int
+}
+
+func walkDOM(n *html.Node, depth int, lines *[]Line) {
+	switch n.Type {
+	case html.ElementNode:
+		// *lines = append(*lines, Line{Text: n.Data, Depth: depth})
+		for _, a := range n.Attr {
+			if a.Key == "class" {
+				fmt.Println(n.Data, a.Key, a.Val)
 			}
+		}
+	case html.TextNode:
+		trimmed := strings.TrimSpace(n.Data)
+		if trimmed != "" {
+			*lines = append(*lines, Line{Text: trimmed, Depth: depth})
 		}
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		extractPTexts(c, results)
+		walkDOM(c, depth+1, lines)
 	}
+}
+
+func getPrimaryScreenSize() (int, int) {
+	n := screenshot.NumActiveDisplays()
+	if n <= 0 {
+		log.Fatal("no display detected")
+	}
+
+	// ここでは一番左上のディスプレイを取得
+	bounds := screenshot.GetDisplayBounds(0)
+	width := bounds.Dx()
+	height := bounds.Dy()
+	return width, height
 }
